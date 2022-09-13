d5c1a1649c63029782669eff916f74ec1027fd10
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-09-13 12:04:52 +0600

    sign up activity to log in activity done

--- app/src/main/java/com/example/sharecaredesign/SignUpActivity.java
+++ app/src/main/java/com/example/sharecaredesign/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.sharecaredesign;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends Activity {
     @Override
@@ -9,5 +11,10 @@ public class SignUpActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+    }
+
+    public void submitSignUp(View view) {
+        Intent intent = new Intent(this,LogINActivity.class);
+        startActivity(intent);
     }
 }
